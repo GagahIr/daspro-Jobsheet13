604a6cc1974a7e4a0bc2ef93a82c95c8bcacb481
GagahIr <gagahirlangsa30@gmail.com>
2024-12-05 19:59:12 +0700

    diyah dan ucup lop lop

--- krsfiza.java
+++ krsfiza.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class krsfiza {
     static String[][] krs = new String[100][5];
-    static int data = 0;
-    
+    static int data = 0, totalSKS = 0;
+
     public static void tambahData(Scanner sc) {
         String nama, nim, kodeMatkul, namaMatkul;
         int jumlahSks;
@@ -22,7 +22,9 @@ public class krsfiza {
                 namaMatkul = sc.nextLine();
                 System.out.print("Jumlah SKS: ");
                 jumlahSks = sc.nextInt();
+
                 sc.nextLine();
+
                 if (jumlahSks >= 1 && jumlahSks <= 3) {
                     System.out.println("Data berhasil ditambahkan");
                     break;
@@ -31,36 +33,43 @@ public class krsfiza {
                     continue;
 
                 }
-                
+
             }
-            
+            if (totalSKS >= 24) {
+                System.out.println("Total sks tidak boleh lebih dari 24");
+                break;
+            }
+            totalSKS += jumlahSks;
+
             krs[data][0] = nim;
             krs[data][1] = nama;
             krs[data][2] = kodeMatkul;
             krs[data][3] = namaMatkul;
             krs[data][4] = Integer.toString(jumlahSks);
             data++;
-            
+
             System.out.print("Masukkan matkul lainnya (y/t)? ");
             String pilih = sc.nextLine();
-            
+
             if (pilih.equalsIgnoreCase("t")) {
                 break;
             }
         }
         System.out.println("Total SKS yang diambil: " + hitungTotalSks(nim));
     }
-    public static int hitungTotalSks(String nim){
-        int totalSKS = 0;
+
+    public static int hitungTotalSks(String nim) {
+        int totalDiAmbil = 0;
         for (int i = 0; i < data; i++) {
             if (krs[i][0].equals(nim)) {
-                totalSKS += Integer.parseInt(krs[i][4]);
+                totalDiAmbil += Integer.parseInt(krs[i][4]);
             }
         }
-        return totalSKS;
+        return totalDiAmbil;
     }
+
     public static void tampilData(Scanner sc) {
-        
+
         String nim;
         int totalSKS = 0;
 
@@ -70,18 +79,34 @@ public class krsfiza {
         nim = sc.nextLine();
         // sc.nextLine();
 
-        // boolean search = false;
         System.out.println("\ndata mata kuliah");
         System.out.printf("%-12s %-20s %-10s %-25s %-10s\n", "NIM", "Nama", "Kode MK", "Nama Mata Kuliah", "SKS");
         for (int i = 0; i < data; i++) {
-            if (nim.equals(krs[i][0])) { 
+            if (nim.equals(krs[i][0])) {
                 // search = true;
                 totalSKS += Integer.parseInt(krs[i][4]);
-                System.out.printf("%-12s %-20s %-10s %-25s %-10s\n", krs[i][0], krs[i][1], krs[i][2], krs[i][3], krs[i][4]);
-            } 
+                System.out.printf("%-12s %-20s %-10s %-25s %-10s\n", krs[i][0], krs[i][1], krs[i][2], krs[i][3],
+                        krs[i][4]);
+            }
         }
-        
+
         System.out.println("Total SKS: " + hitungTotalSks(nim));
+    }
+
+    public static void analisisJumlah() {
+        int hitungMahasiswa = 0;
+
+        for (int i = 0; i < data; i++) {
+            String nim = krs[i][0];
+            int totalSksMahasiswa = hitungTotalSks(nim);
+
+            if (totalSksMahasiswa < 20) {
+                hitungMahasiswa++;
+            }
+        }
+
+        System.out.println("--- Analisis Data KRS ---");
+        System.out.println("Jumlah mahasiswa yang mengambil SKS kurang dari 20 : " + hitungMahasiswa);
     }
 
     public static void main(String[] args) {
@@ -95,17 +120,18 @@ public class krsfiza {
             System.out.println("4. Keluar");
             System.out.print("Pilih menu: ");
             pilihan = sc.nextInt();
-            sc.nextLine(); 
+            sc.nextLine();
 
             switch (pilihan) {
                 case 1:
                     tambahData(sc);
                     break;
                 case 2:
-                    tampilData(sc);;
+                    tampilData(sc);
+                    ;
                     break;
                 case 3:
-                    // analisisJumlah(sc);
+                    analisisJumlah();
                     break;
                 case 4:
                     System.out.println("Terima kasih!");
